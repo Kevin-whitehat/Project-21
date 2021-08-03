d79f3117bc49a825db2af79f95299c9f7fa704d1
Kevin-whitehat <85566492+Kevin-whitehat@users.noreply.github.com>
2021-08-03 08:51:35 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,6 +8,9 @@ var ball;
 var ground;
 var leftSide;
 var rightSide;
+var barrerLeft;
+var barrierRight;
+var barrierTop;
 
 function preload() {
 
@@ -21,20 +24,29 @@ function setup() {
 	world = engine.world;
 
 	//Create the Bodies Here.
+	//groundOptions = { isStatic: true };
+	//ground = Bodies.rectangle(width / 2, 400, width, 20, groundOptions);
+	//World.add(world, ground);
+	//ground = new Ground(400, 390, 400, 20);
 
-	ground = new Ground(400, 330, 800, 10);
-	leftSide = new Ground(400, 300, 10, 50);
-	rightSide = new Ground(500, 300, 10, 50)
+	ground = new Ground(width / 2, 330, width, 10);
+	leftSide = new Ground(400, 320, 10, 50);
+	rightSide = new Ground(550, 230, 10, 200);
+
+	barrierTop = new Ground(width / 2, 600, width, 30);
+	barrierLeft = new Ground(10, 300, 30, 600);
+	barrierRight = new Ground(width - 10, 300, 30, 60)
+
 
 
 	var ball_options = {
 		isStatic: false,
 		restitution: 0.3,
-		friction: 0,
-		density: 1.2
+		//friction: 0,
+		//density: 1.2
 	}
 
-	ball = Matter.Bodies.circle(400, 300, 250, ball_options);
+	ball = Matter.Bodies.circle(80, 300, 20, ball_options);
 	World.add(world, ball);
 
 
@@ -48,18 +60,24 @@ function setup() {
 
 function draw() {
 	rectMode(CENTER);
+	ellipseMode(CENTER);
 	background(0);
 	Engine.update(engine);
 	ellipse(ball.position.x, ball.position.y, 20);
+	//rect(ground.position.x, ground.position.y, width, 20)
 	ground.show();
 	leftSide.show();
 	rightSide.show();
 	drawSprites();
+	force();
+}
+function force() {
 	if (keyDown("UP_ARROW")) {
-		Matter.Body.applyForce(ball, { x: 0, y: 0 }, { x: 0, y: -0.05 });
+		Matter.Body.applyForce(ball, { x: 0, y: 0 }, { x: 0, y: 0.065 });
+		Matter.Body.applyForce(ball, { x: 0, y: 0 }, { x: 0.02, y: 0 });
+		console.log("worked");
 	}
 }
-
 
 
 
